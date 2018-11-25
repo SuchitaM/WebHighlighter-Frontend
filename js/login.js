d21a860e7438380d7072e6b757a46ad9ea2ed59c
Suchita Mudholkar <suchitamudholkar@gmail.com>
2018-11-25 12:33:37 -0800

    Update login.js
    added build inbox

--- js/login.js
+++ js/login.js
@@ -96,18 +96,70 @@ function onBuildMyShelfSuccess(resp)
 
 function buildInbox()
 {
-	
 	//TODO
 	//give req to server to get my messages
 	//get response and parse and build UI
-	// chrome.storage.sync.get('user_id', function(resp){
-	// 		sendRequestToServer(server+"/api/messages/"+resp.user_id,"GET","",onBuildInboxSuccess)	
+
+	// 	chrome.storage.local.get('user_id', function(resp){
+	// 		sendRequestToServer(server+"/api/store/annotations?user_id="+resp.user_id,"GET","",onsuccessOne)	
 	// })
+	var resp = [
+		{
+		"_id": "5bf9e776c7e8a117ec009a8c","from": "5bea19e44a8e7a2ed08d9aad",
+		"to": "5bf110e33343e7262cddd9c3","subject": "first highlight",
+		"annotations": [{
+			"_id": "5bf09448f9bfda3ac0d9c4a0",
+			"ranges": [{ "start": "/div[3]/div[3]/div[4]/div[1]/p[12]", "startOffset": 0, "end": "/div[3]/div[3]/div[4]/div[1]/p[12]/a[3]", "endOffset": 7 }],
+			"quote": "His first double century (204*) was for Mumbai while playing against the visiting Australian team at the Brabourne Stadium in 1998.[1][59] He is the only player to score a century on debut in all three of his domestic first-class tournaments (the Ranji, Irani, and Duleep Trophies).[60] Another double century was an innings of 233* against Tamil Nadu in the semi-finals of the 2000 Ranji T",
+			"text": "vbvb",
+			"uri": "https://en.wikipedia.org/wiki/Sachin_Tendulkar",
+			"date": "2018-11-17T22:20:44.056Z",
+			"user_id": "5bea07490cc7693ebc864c3a"
+		}],
+		"time_stamp": 11
+		},
+		 
+		{
+		"_id": "5bf9e7bcc7e8a117ec009a8d", "from": "5bea19e44a8e7a2ed08d9aad", 
+		"to": "5bf110e33343e7262cddd9c3", "subject": "second highlight", 
+		"annotations": [{
+			"_id": "5bf1d7df9cebe742d8bebe61", 
+			"ranges": [{ "start": "/div[3]/div[3]/div[4]/div[1]/p[4]", "startOffset": 507, "end": "/div[3]/div[3]/div[4]/div[1]/p[4]/a[12]", "endOffset": 16 }], 
+			"quote": "In the final of the 2011 World Cup, Dhoni scored 91 not out off 79 balls handing India the victory  Australia in a Test series. In the Indian Premier L", 
+			"text": "dhoni", 
+			"uri": "https://en.wikipedia.org/wiki/MS_Dhoni",
+			"date": "2018-11-18T21:21:23.838Z",
+			"user_id": "5bea07490cc7693ebc864c3a"
+		}],
+		"time_stamp": 12
+		},
+];
+
+	onBuildInboxSuccess(resp);
 }
 
 function onBuildInboxSuccess(resp)
 {
-	console.log(resp);
+	$(function () {
+		
+		var html = '<div class="container2">';
+		for (var i = 0; i < resp.length; i++) {
+			
+			html += '<div class="container2">';
+			html += '<img src="/arun.jpg" alt="Avatar" style="width:100%;">';
+            html += '<input type="checkbox" id="mycheckbox"> name:' + resp[i].subject + '</input>';
+			html += '<div id="hidden">';
+			html += '<br/><br/>' + '<p>' + resp[i].annotations[0].quote +'</p>';
+			html += '<label style= "position:right" for="mycheckbox">Show/hide</label>';
+		
+			html +='</div>';
+		
+			html += '<span class="time-right">' + resp[i].time_stamp + '</span>';
+		
+		html += '</div>';
+	};
+		$('#inbox_content').html(html);
+	});
 }
 //Swtich tabs in home page
 function showMyShelf()
