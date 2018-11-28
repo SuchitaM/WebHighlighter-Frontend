1dc58184d76ddf57c72fd5dc173ce4f73a4d4244
Y.Arun Ramanathan <y.arunram@gmail.com>
2018-11-28 01:46:11 -0800

    Messaging, Live sharing, UI improvements.
    Messaging, Live sharing, UI improvements.

--- js/content.js
+++ js/content.js
@@ -1,9 +1,13 @@
 jQuery(function ($) {
-	var currentURL = window.location.href;
 	var date = new Date();
 	var server = "https://webharvester.herokuapp.com";
+	var requested_annotation_id = getParams("annotation_id");
+	var currentURL = window.location.href;
+	var messaging_endpoint = requested_annotation_id ? server+"/api/store/annotations/annotation_id/"+requested_annotation_id : server+"/api/store/annotations?url="+currentURL
 	var user_id,username;
 	var annotator = $(document.body).annotator();
+	var socket = io.connect(server);
+	var socket_start = true;
 	finishPreReqs();
 
 	function finishPreReqs()
@@ -31,18 +35,49 @@ jQuery(function ($) {
         'uri': currentURL,
         'date': date,
         'user_id' : user_id,
-        "comment" : "by "+username
+        "comment" : " - "+username
       }
 
     });
 
-    sendRequestToServer(server+"/api/store/annotations?url="+currentURL,"GET",null,onGetHighlights)
+    Annotator.Plugin.StoreLogger = function (element) {
+	  return {
+	    pluginInit: function () {
+	      this.annotator
+	          .subscribe("annotationCreated", function (annotation) {
+	            console.info("The annotation: %o has just been created!", annotation);
+	             if(socket_start){
+	            	console.log("sending anno");
+	            	delete annotation.highlights;
+	              socket.emit('share_annotaton', { annotation: JSON.stringify(annotation)});  
+	            }
+
+	          });
+	    }
+	  }
+	}
+
+ 	annotator.annotator('addPlugin', 'StoreLogger', {});
+    console.log(messaging_endpoint);
+    sendRequestToServer(messaging_endpoint,"GET",null,onGetHighlights)
     }
+
+    socket.on('share_annotaton', function(msg){
+    	console.log("received anno");
+    	var highlight = [JSON.parse(msg.annotation)];
+	  	annotator.annotator('loadAnnotations',highlight);
+	});
+
 
     function onGetHighlights(response){
     	    annotator.annotator('loadAnnotations',response.rows);
     }
     
+    function getParams(name){
+	   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+	      return decodeURIComponent(name[1]);
+	}
+
     function sendRequestToServer(url,method,data,callback)
 	{
 	$.ajax({
--- js/login.js
+++ js/login.js
@@ -3,6 +3,8 @@ document.getElementById("myshelf").addEventListener("click", showMyShelf)
 document.getElementById("inbox").addEventListener("click", showInbox);
 
 var username = "Guest";
+var msg_annotation_id = "";
+var elem = "";
 //If already loggedin then show home page
 chrome.storage.sync.get("loggedin", function(resp)
 {
@@ -53,11 +55,6 @@ function buildHomePage(resp)
 
 function buildMyShelf()
 {
-	//TODO
-	//give req to server to get my highlights
-	//get response and parse and build UI
-		// document.getElementById("myshelf_content").innerHTML = "My highlights";
-	// document.getElementById("myhighlights").innerHTML = "resp";
 	chrome.storage.sync.get('user_id', function(resp){
 			sendRequestToServer(server+"/api/store/annotations/user_id/"+resp.user_id,"GET","",onBuildMyShelfSuccess)	
 	})
@@ -68,100 +65,101 @@ function onBuildMyShelfSuccess(resp)
 	var arrayquotes =[];
 	var length = JSON.stringify(JSON.parse(resp).count);
 	var rows = JSON.parse(resp).rows;
+	console.log(rows);
 	
-	for (i = 0; i < length; i++) 
-	{
-		  console.log(rows[i].quote);
-		  arrayquotes.push(rows[i].quote);
-	}
-
     $(function(){
 
-			var html = '<div class="container2">';
+   		var html = '';
+        for (var i = rows.length-1; i >= 0; i--) {
+            //console.log(resp[i]);
             
-			var html = '<div class="container2">' +'<table>';
-			$.each(arrayquotes, function(key, value){
+            html += '<div class="container2">';
+            
+            html += '<input type="checkbox" id="myshelf_item'+i+'">'+ rows[i].uri + '</input>';
+            
+            html += '<div id="hidden"></br></br>';
+            html += '<div style="float:left"><a href="#" class="redirect" data-href = "'+rows[i].uri+'?annotation_id='+rows[i]._id+'">' + JSON.stringify(rows[i].quote)+'</a></div>';
+            html += '<div class="right"><a href="#modalScreen" class="button modal_annotation" data-ann_id = "'+rows[i]._id+'"><img src="img/forward.png" class="forward" height="130px" data-ann_id = "'+rows[i]._id+'"/></a></div></td>';
+            html += '</div>';
+            html += '<label for="myshelf_item'+i+'">Show/hide</label>';
+             
+            html += '</div>';
+    	};
 			
-		    html += '<tr>';
-			
-		    html += '<td>' + JSON.stringify(value) + '</td>';
-			html += '</tr>';
-			});
-			html += '</table>';
-			html+='</div>';
 
-			$('span1').html(html);
+		html +='<div id="modalScreen">'+'<fieldset class="group">' +'<legend>'+'Select names to forward'+'</legend>'+'</fieldset>'+'</br>';
+
+		html += '<ul class="checkbox">'+'<li>'+'<input type="checkbox" id="c_b" />'+'<label for="c_b">Renu</label><input data-to-id=\"5bf9c428e8c62a1f94300414\" type="button" class="send" value="send"/></li>';
+		html += '<li>'+'<input type="checkbox" id="c$b" value="5bf9c409e8c62a1f94300413"/>'+'<label for="c$b">'+'Arun'+'</label>'+'<input type="button" data-to-id=\"5bf9c409e8c62a1f94300413\"  class="send" value="send"/>'+'</li>'; 
+		html += '<li>'+ '<input type="checkbox" id="c@b" value="5bf9c43de8c62a1f94300415" />'+'<label for="c@b">'+'Pranav'+'</label>'+'<input type="button" data-to-id=\"5bf9c43de8c62a1f94300415\"  class="send" value="send"/>'+'</li>'; 
+
+		html += '<li>'+'<input type="checkbox" id="c%b" value="5bf9c44fe8c62a1f94300416" />'+'<label for="c%b">'+'Suchita'+'</label>'+'<input type="button" data-to-id=\"5bf9c44fe8c62a1f94300416\"  class="send" value="send"/>'+'</li> '+'</ul>';
+		html += '<br/>'+ '<div style="padding-left: 10px">'+'</div>'+'</fieldset>';
+		html += '<a href="#" class="cancel">'+'&times;'+'</a>'+'</div>';
+		html += '<input type="button"="cover" >'+'</div>';
+
+
+		$('span1').html(html).promise().done(function(){
+			var modal_annotation = document.getElementsByClassName("modal_annotation");
+		    data = modal_annotation;
+		    for (var i = 0; i < data.length; i++) {
+			    data[i].addEventListener('click', setModalValue);
+			}
+
+			var send_annotation = document.getElementsByClassName("send");
+		    data = send_annotation;
+		    for (var i = 0; i < data.length; i++) {
+			    data[i].addEventListener('click', postmessage);
+			}
+			});
 		});
+
 }
 
 function buildInbox()
 {
-	//TODO
-	//give req to server to get my messages
-	//get response and parse and build UI
-
-	// 	chrome.storage.local.get('user_id', function(resp){
-	// 		sendRequestToServer(server+"/api/store/annotations?user_id="+resp.user_id,"GET","",onsuccessOne)	
-	// })
-	var resp = [
-		{
-		"_id": "5bf9e776c7e8a117ec009a8c","from": "5bea19e44a8e7a2ed08d9aad",
-		"to": "5bf110e33343e7262cddd9c3","subject": "first highlight",
-		"annotations": [{
-			"_id": "5bf09448f9bfda3ac0d9c4a0",
-			"ranges": [{ "start": "/div[3]/div[3]/div[4]/div[1]/p[12]", "startOffset": 0, "end": "/div[3]/div[3]/div[4]/div[1]/p[12]/a[3]", "endOffset": 7 }],
-			"quote": "His first double century (204*) was for Mumbai while playing against the visiting Australian team at the Brabourne Stadium in 1998.[1][59] He is the only player to score a century on debut in all three of his domestic first-class tournaments (the Ranji, Irani, and Duleep Trophies).[60] Another double century was an innings of 233* against Tamil Nadu in the semi-finals of the 2000 Ranji T",
-			"text": "vbvb",
-			"uri": "https://en.wikipedia.org/wiki/Sachin_Tendulkar",
-			"date": "2018-11-17T22:20:44.056Z",
-			"user_id": "5bea07490cc7693ebc864c3a"
-		}],
-		"time_stamp": 11
-		},
-		 
-		{
-		"_id": "5bf9e7bcc7e8a117ec009a8d", "from": "5bea19e44a8e7a2ed08d9aad", 
-		"to": "5bf110e33343e7262cddd9c3", "subject": "second highlight", 
-		"annotations": [{
-			"_id": "5bf1d7df9cebe742d8bebe61", 
-			"ranges": [{ "start": "/div[3]/div[3]/div[4]/div[1]/p[4]", "startOffset": 507, "end": "/div[3]/div[3]/div[4]/div[1]/p[4]/a[12]", "endOffset": 16 }], 
-			"quote": "In the final of the 2011 World Cup, Dhoni scored 91 not out off 79 balls handing India the victory  Australia in a Test series. In the Indian Premier L", 
-			"text": "dhoni", 
-			"uri": "https://en.wikipedia.org/wiki/MS_Dhoni",
-			"date": "2018-11-18T21:21:23.838Z",
-			"user_id": "5bea07490cc7693ebc864c3a"
-		}],
-		"time_stamp": 12
-		},
-];
-
-	onBuildInboxSuccess(resp);
+	chrome.storage.sync.get('user_id', function(resp){
+        sendRequestToServer(server+"/api/messages/"+resp.user_id,"GET","",onBuildInboxSuccess)  
+	})
 }
 
-function onBuildInboxSuccess(resp)
+function onBuildInboxSuccess(response)
 {
-	$(function () {
-		
-		var html = '<div class="container2">';
-		for (var i = 0; i < resp.length; i++) {
-			
-			html += '<div class="container2">';
-			html += '<img src="/arun.jpg" alt="Avatar" style="width:100%;">';
-            html += '<input type="checkbox" id="mycheckbox"> name:' + resp[i].subject + '</input>';
-			html += '<div id="hidden">';
-			html += '<br/><br/>' + '<p>' + resp[i].annotations[0].quote +'</p>';
-			html += '<label style= "position:right" for="mycheckbox">Show/hide</label>';
-		
-			html +='</div>';
-		
-			html += '<span class="time-right">' + resp[i].time_stamp + '</span>';
-		
-		html += '</div>';
-	};
-		$('#inbox_content').html(html);
-	});
+    console.log(response);
+    resp = response.rows;
+    
+    $(function () {
+
+        var html = '';
+        for (var i = resp.length-1; i >= 0; i--) {
+            
+            html += '<div class="container2">';
+            
+            html += '<img src="img/jon-snow.jpg" alt="Avatar" style="width:60px;height:60px"/>';          
+
+            html += '<input type="checkbox" id="mycheckbox'+i+'">'+ resp[i].username + ':-  ' + resp[i].annotations[0].uri + '</input>';
+            
+            html += '<div id="hidden"></br></br>';
+            html += '<div style="float:left"><a href="#" class="redirect" data-href = "'+resp[i].annotations[0].uri+'?annotation_id='+resp[i].annotation_id+'">'+ resp[i].annotations[0].quote + '</a></div>';
+            html += '</div>';
+            html += '<label for="mycheckbox'+i+'">Show/hide</label>';
+            
+            html += '<div class="time-right">' + resp[i].timestamp + '</div>';
+            
+            html += '</div>';
+    	};
+        
+        $('#inbox_content').html(html).promise().done(function(){
+        	var redirect = document.getElementsByClassName("redirect");
+		    for (var i = 0; i < redirect.length; i++) {
+			    redirect[i].addEventListener('click', redirect_to_link);
+			}
+
+        });
+    });
 }
-//Swtich tabs in home page
+
+//Switch tabs in home page
 function showMyShelf()
 {
 	document.getElementById("myshelf_content").style.display = "block";
@@ -174,11 +172,38 @@ function showInbox()
 	document.getElementById("inbox_content").style.display = "block";
 }
 
+function postmessage()
+{
+	console.log("posting msg");
+	var to = event.target.getAttribute("data-to-id");
+	//var to_name = event.target.getAttribute("data-to-name");
+	var subject = "Sample message";
+	chrome.storage.sync.get('user_id', function(resp){
+			var data = {};
+			data.from = resp.user_id;
+			data.to = to;
+			data.subject = subject;
+			data.username = username;
+			data.timestamp = new Date();
+			data.annotation_id = msg_annotation_id;
+			console.log(data);
+			sendRequestToServer(server+"/api/messages/","POST",data,onPostMessageSuccess)	
+	})
+}
 
+function onPostMessageSuccess(resp)
+{
+	console.log(resp);
+}
 
+function setModalValue(event)
+{
+	console.log("modal");
+	msg_annotation_id = event.target.getAttribute("data-ann_id");
+}
 
-
-
-
-	
-	
+function redirect_to_link()
+{
+	url = event.target.getAttribute("data-href");
+	chrome.tabs.create({url: url});
+}
